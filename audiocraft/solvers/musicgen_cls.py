ac4bd0e1561c974b5053874d5186cfd5d69d8839
sean1572 <shperry@ucsd.edu>
2025-06-12 15:43:29 -0700

    Adds training + eval run for cls

--- audiocraft/solvers/musicgen_cls.py
+++ audiocraft/solvers/musicgen_cls.py
@@ -14,6 +14,9 @@ import librosa
 import math
 import omegaconf
 import torch
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
@@ -31,7 +34,10 @@ class CLSModel(torch.nn.Module):
         for param in self.model.parameters():
             param.requires_grad = False
 
-        TODO_FIND_LAYER_SIZE = 100
+        # All dimensions that aren't batch for final classification
+        # Does this make sense? Probably not. 
+        # Will need to dig deeper into musicgen to figure out most useful layer
+        TODO_FIND_LAYER_SIZE = 2048 * 250 * 4 
         self.linear = torch.nn.Linear(TODO_FIND_LAYER_SIZE, num_species)
 
     def __del__(self):
@@ -39,9 +45,9 @@ class CLSModel(torch.nn.Module):
             param.requires_grad = True
     
     def forward(self, audio_tokens, condition_tensors):
-        print(self.model)
+        print(audio_tokens.shape)
         model_output = self.model.compute_predictions(audio_tokens, [], condition_tensors)
-        logits = model_output.logits
+        logits = model_output.logits.reshape(-1, 2048 * 250 * 4) #TODO WHAT TO DO HERE...
         print(logits.shape)
         out = self.linear(logits)
         return out
@@ -50,8 +56,10 @@ def one_hot(tensor, num_classes, on_value=1., off_value=0.):
     """Return one hot tensor of length num_classes
     """
     tensor = tensor.long().view(-1, 1)
-    return torch.full((tensor.size()[0], num_classes), off_value, device=tensor.device) \
-                .scatter_(1, tensor, on_value)
+    return torch.clamp(
+        torch.full((tensor.size()[0], num_classes), off_value, device=tensor.device) \
+                .scatter_(1, tensor, on_value).sum(dim=0),
+        min=0, max=1)
 
 class MusicGenSolverCLS(MusicGenSolver):
     def __init__(self, cfg: omegaconf.DictConfig):
@@ -72,9 +80,17 @@ class MusicGenSolverCLS(MusicGenSolver):
         labels = []
         
         for i in range(len(batch)):
-            audio, sr = librosa.load(path=batch[i]["filepath"], sr=32_000)
+            y, sr = librosa.load(path=batch[i]["filepath"], sr=32_000)
             label = one_hot(torch.Tensor(batch[i]["ebird_code_multilabel"]), self.num_classes)
-            audios.append(torch.Tensor(audio))
+            start = 0
+
+            # Select a random 5 second segment
+            # From https://github.com/UCSD-E4E/pyha-analyzer-2.0/blob/main/pyha_analyzer/preprocessors/spectogram_preprocessors.py
+            if y.shape[-1] > (sr * 5):
+                start = np.random.randint(0, y.shape[-1] - (sr * 5))
+            else:
+                y = np.pad(y, (sr * 5) - y.shape[-1])
+            audios.append(torch.Tensor(y[start : start + (sr * 5)]))
             labels.append(label)
 
         # print(torch.vstack(audios).shape,  torch.vstack(labels).shape, flush=True)
@@ -86,7 +102,7 @@ class MusicGenSolverCLS(MusicGenSolver):
         
         for split in self.ds.keys():
             self.ds[split] = self.ds[split].with_format(None, columns=["filepath", "ebird_code_multilabel"])
-            self.cls_dataloaders[split] = DataLoader(self.ds[split], batch_size=5, collate_fn=self.collate)
+            self.cls_dataloaders[split] = DataLoader(self.ds[split], batch_size=1, collate_fn=self.collate)
 
         self.num_classes = 21
 
@@ -115,22 +131,53 @@ class MusicGenSolverCLS(MusicGenSolver):
 
         #Step 2: Get a model prepped
         self.build_temp_model()
+        self.cls_model = self.cls_model.cuda()
 
         # Short training loop, should only do an epoch as a quick test
         # TODO
 
+        # Train Loop
+        criterion = nn.BCEWithLogitsLoss()
+        optimizer = optim.Adam(self.cls_model.parameters(), lr=0.001)
+        self.cls_model.train()
+        for i, batch in enumerate(self.cls_dataloaders["train"]):
+            # Step 3: Data preprocessing to fake a dataset, because I am lazy
+            # TODO: turn this into a function call, need for training too
+            audio_data, labels = self.transform_hugging_face_ds(batch)
+            print([self.get_metadata(item["filepath"]) for item in batch])
+            condition_tensors, audio_tokens, padding_mask = self._prepare_tokens_and_attributes(
+                (audio_data, [self.get_metadata(item["filepath"]) for item in batch]))
+            print(condition_tensors)
+            optimizer.zero_grad()
+            output = self.cls_model(audio_tokens, condition_tensors)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            break #TODO REMOVE
+
         # Evaluation Loop
+        self.cls_model.eval()
         for i, batch in enumerate(self.cls_dataloaders["test_5s"]):
             # Step 3: Data preprocessing to fake a dataset, because I am lazy
             # TODO: turn this into a function call, need for training too
             audio_data, labels = self.transform_hugging_face_ds(batch)
+            print(batch)
+            print(audio_data)
             condition_tensors, audio_tokens, padding_mask = self._prepare_tokens_and_attributes(
                 (audio_data, [self.get_metadata(item["filepath"]) for item in batch]))
 
             # TODO Get metrics from model performance on soundscapes / xc validation split?
             
-            print(audio_tokens.dtype, condition_tensors)
-            self.cls_model(audio_tokens, condition_tensors)
+            description_tesnor = condition_tensors["description"][0].to(torch.float32)
+            condition_tensors["description"] = (description_tesnor, condition_tensors["description"][1])
+            print(audio_tokens.shape, condition_tensors["description"][0].shape)
+            print(self.cls_model(audio_tokens, condition_tensors).shape, labels.shape)
+
+            output = self.cls_model(audio_tokens, condition_tensors)
+            loss = criterion(output, labels)
+            print(loss)
+            break #TODO REMOVE
 
         del self.cls_model
 
